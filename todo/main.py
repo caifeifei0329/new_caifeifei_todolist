6b530995451c560beff140a0dee7c9905fb47df8
caifeifei0329 <2432652339@qq.com>
2018-05-29 21:12:20 +0800

    界面

--- todo/main.py
+++ todo/main.py
@@ -1,11 +1,17 @@
 
-from flask import  Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
-def hello():
-    return 'helloworld'
+def index():
+    todo={
+        '测试一',
+        '测试二',
+        '测试三',
+        '测试四',
+        '测试五'
+    }
+    return render_template('index.html',data=todo)
 if __name__=='__main__':
 
     app.run(debug=True);
