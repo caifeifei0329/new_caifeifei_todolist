78e7d8075fa2eb04c77558ee66fedd3d6f089acf
caifeifei0329 <2432652339@qq.com>
2018-05-30 20:37:47 +0800

    add test.db

--- todo/db.py
+++ todo/db.py
@@ -1,9 +1,28 @@
+import sqlite3
+
+
 class TodoDb():
     def read_all(self):
-        return  {
-            '测试一',
-            '测试二',
-            '测试三',
-            '测试四',
-            '测试五',
-        }
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        data=cursor.execute('select content from todo')
+        data=data.fetchall()
+        data=[d[0] for d in data]
+        return data
+    # def insert_db(self):
+    #     conn = sqlite3.connect('test.db')
+    #     cursor = conn.cursor()
+    #     data=cursor.execute('insert into todo(content) values(?)')
+
+
+    def init_db(self):
+        conn=sqlite3.connect('test.db')
+        cursor=conn.cursor()
+        cursor.execute('create table todo(id integer primary key AUTOINCREMENT,content varchar(200) )')
+        cursor.close()
+        conn.commit()
+        conn.close()
+if __name__=='__main__':
+    db=TodoDb()
+    # db.init_db()
+    db.read_all()
